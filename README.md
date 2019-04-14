26ed06936834dc2123b1787c9e07a9f7947b7593
Jérémy Creola <jeremy.creola@gmail.com>
2019-04-15 00:04:42 +0200

    Fichiers story teller

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# story-teller
+﻿# Story Teller
+
+## Qu'est-ce que c'est ?
+
+Ce story teller a été réalisé en quelques minutes lors d'une soirée entre amis dans un but purement divertissant. A l'aide de Prompt et d'Alert JavaScript, je pose plusieurs questions. Chacun avait un portable pour faire l'exercice et devait répondre puis partageait son histoire avec les autres.
+
+J'ai réalisé ce petit jeu afin de montrer à mes proches étant totalement hors du monde du code qu'il était facile de faire des petites choses amusantes en quelques minutes seulement et que le codage n'est pas quelque chose de rébarbatif comme ils pourraient le penser. 
+
+## Etat 
+
+- Terminé mais quelques améliorations peuvent y être apportées
+
+## Technologies utilisées
+
+- JavaScript
+
+## Améliorations à venir
+
+- Je compte créer plusieurs histoire (dans le même fichier JS évidemment) afin que tout le monde en obtienne une différente selon une donnée rentrée au préalable (par ex. homme ou femme) ce qui rendrait le jeu plus amusant. 
+
+## Résultat :
+
+- Il est possible de faire ce petit jeu dans son état actuel ici : 
---
+++ script.js
@@ -0,0 +1,14 @@
+(function() {
+
+let prenom = prompt ("Bonjour ! Quel est ton prénom ?");
+let sexe = prompt ("Es tu un homme ou une femme ?");
+let plat = prompt ("Quel est ton plat favori ?");
+let kilos = prompt ("Quel est ton poids (tu peux me mentir, pas de soucis)");
+let bestFriend = prompt ("Quel est le prénom de ton/ta meilleur(e) ami(e)");
+let musique = prompt ("Ruben nous a parlé dans son Workshop de Programmation Neuro Linguistique, peux-tu me donner les initiales ?");
+let sport = prompt ("Quel sport trouve-tu le plus ridicule");
+let peur = prompt ("De quoi as-tu peur ? Moi c'est les serpents");
+
+alert ("Voici " + prenom + ",  un " + plat + " de " + kilos + " kilos. " + prenom + " est un " + sexe + ", en tout cas c'est ce qu'il nous dit. Car " + bestFriend + " nous a dit la vérité, le rêve de " + prenom + " est d'être du sexe opposé. En tout cas, " + prenom + " essaie de se faire passer pour un vrai bonhomme en écoutant son groupe préféré, " + musique + " ." + prenom + " a toujours rêvé de devenir joueur professionnel de " + sport + " . Malheureusement il n'y est pas arrivé à cause de sa peur des " + peur + " , quelle triste destin.");
+
+})();
